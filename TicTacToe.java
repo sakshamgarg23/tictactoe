fc4bc0c0283e04fb886ca653323771f41a672c29
sakshamgarg23 <99506819+sakshamgarg23@users.noreply.github.com>
2024-10-04 15:43:04 +0530

    [saksham] MOD: added rest features for tictactoe

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,4 @@
-package classPractice;
+import java.util.*;
 class TicTacToe{
 private static char[][] board;
 private static char  Currplayer;
@@ -7,35 +7,55 @@ public static void main(String[] args){
 
     initializeBoard();
     printBoard();
+    moveofPlayers();
+    while(true){
+    }
+   
 
-}
+    }
 
-private static void initializeBoard(){
-board=new char[3][3];
-Currplayer='X';
-for(int i=0;i<board.length;i++){
-for(int j=0;j<board.length;j++){
+    private static void moveofPlayers(){
+    Scanner sc= new Scanner(System.in);
+    while(true){
+    int row,colm;
+    row=sc.nextInt();
+    colm=sc.nextInt();
+    if(row>=0 && row<=2 && colm>=0 && colm<=2 && board[row][colm]=='-'){
+    board[row][colm]=Currplayer;
+    break;
+    }
+
+    else{
+    System.out.print("input not valid please try again");}
+
+    }
+    }
+    private static void initializeBoard(){
+    board=new char[3][3];
+    Currplayer='X';
+    for(int i=0;i<board.length;i++){
+    for(int j=0;j<board.length;j++){
     board[i][j]='-';
         }
     }
  }
 
- private static void printBoard(){
- //System.out.print("board");
- for(int i=0;i<board.length;i++){
- for(int j=0;j<board.length;j++){
- System.out.print(board[i][j]+ " ");
+    private static void printBoard(){
+    //System.out.print("board");
+    for(int i=0;i<board.length;i++){
+    for(int j=0;j<board.length;j++){
+    System.out.print(board[i][j]+ " ");
 }
-System.out.println(" ");
+    System.out.println(" ");
 }
 }
-
-public static char[][] getBoard() {
+ 
+    public static char[][] getBoard() {
     return board;
 }
 
-public static void setBoard(char[][] board) {
-    TicTacToe.board = board;
+    public static void setBoard(char[][] board) {
+    TicTacToe.board = board ;
 }
 
 public static char getCurrplayer() {
